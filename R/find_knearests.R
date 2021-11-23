916089ce8bd29f616e8c8492b22ec25d44c4036c
brandazzle <43732989+brandazzle@users.noreply.github.com>
2021-11-23 10:19:58 -0800

    Fix error in min(series)
    Fix error "Error in max(series) : invalid 'type' (list) of argument" when running find_knearest on a numeric input list

--- R/find_knearests.R
+++ R/find_knearests.R
@@ -1,6 +1,6 @@
-#Author: Antonio, Fabio Di Narzo. Last Modified $Date: 2005-12-02 17:15:39 +0100 (ven, 02 dic 2005) $
+#Author: Antonio, Fabio Di Narzo. Last Modified $Date: 2021-11-23 10:15:39 -0500 (ven, 23 nov 2021) $
 find_knearests <- function(series, m, d, t, eps, ref, k, s){
-	x <- ( series - min(series) ) / (diff(range(series)))
+	x <- ( series - min(unlist(series)) ) / (diff(range(series)))
 	eps <- eps/diff(range(series))
 	res <- numeric(ref*k)
 	res <- .C("find_knearests", series=as.double(x), m=as.integer(m), d=as.integer(d), t=as.integer(t), length=as.integer(length(x)), eps=as.double(eps),ref=as.integer(ref), k=as.integer(k), s=as.integer(s), res=as.integer(res), PACKAGE="tseriesChaos")[["res"]]
